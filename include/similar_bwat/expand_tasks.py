e87c17d2b1826813d6bfb0d1af1a889b23e7f2e6
rusrom <19729227+rusrom@users.noreply.github.com>
2025-09-06 17:14:07 +0300

    similar to bwat

---
+++ include/similar_bwat/expand_tasks.py
@@ -0,0 +1,8 @@
+def make_list():
+    return [[1], [2], [3], [4]]
+
+
+def consume_value(expanded_param, fixed_param):
+    print(fixed_param)
+    print(expanded_param)
+    return [expanded_param]
