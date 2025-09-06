db470779e7c16b68dd16ed127624702fb62c296e
rusrom <19729227+rusrom@users.noreply.github.com>
2025-09-06 12:05:27 +0300

    Working example

---
+++ dags/dag_factory.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+from airflow.models.dag import DAG
+from airflow.configuration import conf as airflow_conf
+from dagfactory import load_yaml_dags
+
+config_dir = Path(airflow_conf.get("core", "dags_folder")) / "configs"
+config_file = str(config_dir / "example_dag_factory.yml")
+
+# load_yaml_dags(globals_dict=globals(), dags_folder=config_dir)
+load_yaml_dags(globals_dict=globals(), config_filepath=config_file)
+# load_yaml_dags(globals_dict=globals(), dags_folder=airflow_conf.get("core", "dags_folder"), config_filepath=config_dir)
+
+
+if __name__ == "__main__":
+    print(config_dir)
