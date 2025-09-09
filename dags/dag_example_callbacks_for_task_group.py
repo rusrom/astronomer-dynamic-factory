8d405b3ef97161bcd3f8b898d7a47d7ab0a82df6
rusrom <19729227+rusrom@users.noreply.github.com>
2025-09-09 20:49:13 +0300

    callback at defalut_args level to pass for all tasks

--- dags/dag_example_callbacks_for_task_group.py
+++ dags/dag_example_callbacks_for_task_group.py
@@ -2,3 +2,9 @@ def callback_for_tg_say_hello(context):
     print("+++++++++++++ tg +++++++++++++++ hello from callback for task_group")
     print(context)
     print("+++++++++++++ tg +++++++++++++++ end of callback for task_group")
+
+
+def defaut_callback_for_all_tasks(context):
+    print("************* task ************** hello from default callback for all tasks")
+    print(context)
+    print("************* task ************** end of default callback for all tasks")
