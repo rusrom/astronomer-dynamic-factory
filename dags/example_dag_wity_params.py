4833c5483d0e3bcad2913f5c1ab93195bc7ed02a
rusrom <19729227+rusrom@users.noreply.github.com>
2025-09-09 16:02:58 +0300

    parameters and getting theim in tasks

---
+++ dags/example_dag_wity_params.py
@@ -0,0 +1,21 @@
+# import os
+from pathlib import Path
+
+# The following import is here so Airflow parses this file
+# from airflow import DAG
+from airflow.configuration import conf as airflow_conf
+from dagfactory import load_yaml_dags
+
+# DEFAULT_CONFIG_ROOT_DIR = "/usr/local/airflow/dags/"
+# CONFIG_ROOT_DIR = Path(os.getenv("CONFIG_ROOT_DIR", DEFAULT_CONFIG_ROOT_DIR))
+
+# config_file = str(CONFIG_ROOT_DIR / "kpo.yml")
+# load_yaml_dags(
+#     globals_dict=globals(),
+#     config_filepath=config_file,
+# )
+
+config_dir = Path(airflow_conf.get("core", "dags_folder")) / "configs"
+config_file = str(config_dir / "example_dag_wity_params.yml")
+
+load_yaml_dags(globals_dict=globals(), config_filepath=config_file)
---
+++ include/similar_bwat/sample.py
@@ -0,0 +1,87 @@
+import csv
+import os
+from datetime import datetime, timedelta
+from random import randint
+from typing import Any
+
+try:
+    from airflow.providers.standard.operators.python import get_current_context
+except ImportError:
+    from airflow.operators.python import get_current_context
+
+
+def build_numbers_list():
+    return [2, 4, 6]
+
+
+def some_number():
+    return randint(0, 100)
+
+
+def double(number: int):
+    result = 2 * number
+    print(result)
+    return result
+
+
+def multiply(a: int, b: int) -> int:
+    result = a * b
+    print(result)
+    return result
+
+
+#    added_values = add.expand(x=first_list(), y=second_list())
+
+
+def double_with_label(number: int, label: bool = False):
+    result = 2 * number
+    if not label:
+        print(result)
+        return result
+    else:
+        label_info = "even" if number % 2 else "odd"
+        print(f"{result} is {label_info}")
+        return result, label_info
+
+
+def extract_last_name(full_name: str):
+    name, last_name = full_name.split(" ")
+    print(f"{name} {last_name}")
+    context = get_current_context()
+    context["custom_mapping_key"] = name
+    return last_name
+
+
+def one_day_ago(execution_date: datetime):
+    return execution_date - timedelta(days=1)
+
+
+def read_params(params: dict[str, Any]) -> None:
+    print("params: ", params)
+    print("model_version:", params["model_version"])
+    print("my_param:", params["my_param"])
+
+
+def generate_data():
+    print("Produced data to file:///$AIRFLOW_HONE/data.csv")
+    data_dir = os.environ.get("AIRFLOW_HONE", "/usr/local/airflow")
+    file_path = os.path.join(data_dir, "data.csv")
+
+    with open(file_path, "w") as f:
+        f.write("id,value\n1,42\n2,43\n")
+
+    print(f"Produced data to file://{file_path}")
+
+
+# def object_storage_ops(my_obj_storage) -> None:
+#     try:
+#         from airflow.sdk import ObjectStoragePath
+#     except ImportError:
+#         from airflow.io.path import ObjectStoragePath
+
+#     assert isinstance(my_obj_storage, ObjectStoragePath)
+#     with my_obj_storage.open("rb") as f:
+#         text = f.read().decode("utf-8")
+#         reader = csv.reader(text.splitlines(), delimiter=",")
+#         rows = list(reader)
+#         print(rows)
